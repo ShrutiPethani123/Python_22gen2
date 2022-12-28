63874073e28fd43d3c56a91e8c6e21cd38a4c95d
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2022-12-28 19:47:49 +0530

    input

---
+++ UserInput.py
@@ -0,0 +1,71 @@
+# print("Enter a no:")
+# a=input()
+# print("Value of a is: ",a)
+
+# b=input("Enter a no:")
+# print("Value of b is: ",b)
+# print(type(b)) 
+
+#  By default input() function -> take string value
+
+"""
+-> Type Casting
+
+str()
+int()
+float()
+bool()
+
+"""
+
+# a='python'
+# b='56'
+# print(type(a))
+# print(type(b))
+
+# # print(type(int(a)))
+# print(type(int(b)))
+# b=int(b)
+# print(type(b))
+
+# a=int(input("Enter a no:"))
+# print("Number is: ",a , " and type is ",type(a))
+
+# a=float(input("Enter a no:"))
+# print("Number is: ",a , " and type is ",type(a))
+
+# b=100
+# print(type(b))
+# b=str(b)
+# print(type(b))
+
+# b="jio"
+# b=56.47
+# b=89
+# b=-67
+b=0
+print(b,type(b))
+b=bool(b)
+print(b,type(b))
+
+
+
+"""
+1. Make one form for registraction.
+
+fname
+lname
+age
+contact no
+email address
+address
+branch
+year/sem
+date of birth
+spi/cpi
+
+
+2. Take two Number from user and print Addition , subtraction , mul, div , mod and average.(+ , - , * , / , %)
+
+"""
+

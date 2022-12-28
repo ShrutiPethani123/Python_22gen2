6317a8d8f2b48bd70dcefeef4e5db32b3d59aff1
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2022-12-28 18:54:54 +0530

    init

---
+++ first.py
@@ -0,0 +1,103 @@
+print("Good Morning",end="\n")
+print("Royal!!!")
+print("Good Morning",end=" Royal")
+print(" Technosoft")
+
+# single line comment 
+
+"""
+multi line 
+comment
+"""
+
+'''
+multi line comment
+'''
+
+print('Good Morning',end=" Royal")
+print(" Technosoft")
+
+
+"""
+
+" == ' -> True
+
+"""
+
+print("-------------------------------------")
+print("Today is Wednesday")
+print('Today is Wednesday')
+print("Today" ,'is' ,'Wednesday')
+
+# This is Good's Day.....
+print("This is Good's day....")
+print("This is Good's day....")
+print('This is Good\"s day....Good\'s day')
+
+"""
+\'
+\"
+\n
+\t
+
+
+"""
+print("Today is \nMonday")
+print("Today is\t Friday!!")
+
+# Today is \n day
+print("Today is \\n day")
+
+
+"""
+
+variable
+
+int
+float
+str
+bool
+complex
+
+"""
+
+
+# int a=10
+a=10
+print(a)
+print(type(a))
+
+a=457467656876890640983265983475029846732894672034986
+print(a)
+print(type(a))
+
+a=34.78979868797385798659834683
+print(a)
+print(type(a))
+
+b='r'
+print(b)
+print(type(b))
+
+b="python"
+print(b,type(b))
+
+a=True
+b= False
+# true false -> invalid
+print(a,type(a))
+
+d=3+5j
+print(d,type(d))
+
+"""
+complex = real + img
+
+real =3
+img = 5
+
+j^2 = -1
+"""
+print("Real Part: ",d.real)
+print("Img Part: ",d.imag)
+
